538df5e98fae37208e3dc0241035ee187b24f838
Saurabh250 <65411015+Saurabh250@users.noreply.github.com>
2021-07-02 01:03:10 +0530

    Add files via upload

---
+++ model/CourseData.java
@@ -0,0 +1,34 @@
+
+package com.rajendra.courseapp.model;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class CourseData {
+
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("properties")
+    @Expose
+    private Properties properties;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+}
